7c2941137b2c853dddce124caed7313143819fa8
Angel Mariscurrena <mariscurrenalae@gmail.com>
2023-01-12 21:51:26 -0600

    Adding the color change event into JS
    Signed-off-by: Angel Mariscurrena <mariscurrenalae@gmail.com>

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ const interval = setInterval(() =>{
     date.innerHTML = `${day} ${monthNames[month]} ${year}`;
 
 }, 1000);
+
+//Creating the constant for the object btn
+const boton = document.querySelector('#btn');
+//Some variables for CSS Style
+var pagina = document.body;
+//Calling an Event.
+boton.addEventListener('click', function () {
+    if(pagina.style.background == "white"){
+        console.log("If_Part");
+        pagina.style.background = "linear-gradient(180deg, rgb(0, 0, 0) 0%, rgb(0, 0, 10) 71%, rgb(0, 0, 30) 100%)";
+        time.style.color = "rgb(230, 230, 230)";
+        date.style.color = "rgb(255, 227, 185)";
+    }else{
+        pagina.style.background = "white";
+        time.style.color = "rgb(0,0,100)";
+        date.style.color = "rgb(0,0,51)";
+    }
+});
